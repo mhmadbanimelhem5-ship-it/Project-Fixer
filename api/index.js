d0ab72c0afecf89c31a5a1aac61cdf88d98b3c8a
mhmadbanimelhem5-ship-it <mhmadbanimelhem5@gmail.com>
2026-08-11 21:07:14 +0000

    fix ESM to CJS warning

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
-export default function handler(req, res) {
+function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   return res.status(200).json({
     status: 'ok',
-    message: 'API is running!',
+    message: 'API is running! No more 404!',
     url: req.url,
+    method: req.method,
     timestamp: new Date().toISOString()
   });
 }
+
+module.exports = handler;
+module.exports.default = handler;
+export default handler;
